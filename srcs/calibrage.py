45e31e1a4e7f1db7c7f74d2a51b7409ec3135e00
Erwan TOUX <erwan.toux@mobile2-17.cpe.lan>
2023-09-29 17:50:48 +0200

    fin seance 2

--- srcs/calibrage.py
+++ srcs/calibrage.py
@@ -4,22 +4,45 @@ from matplotlib import pyplot as plt
 from typing import List, Union, Dict, Tuple
 from PIL import Image
 
-from srcs.utils.utils import read_image
-from srcs.utils.constants import CHESSBOARD_SIZE
+from srcs.utils.utils import read_image, get_A_matrix, get_parameters_from_L
+from srcs.utils.constants import CHESSBOARD_SIZE, P_POINT_X, P_POINT_Y
 
+SAVE_IMAGES = True
 
-SAVE_IMAGES = False
 
 def calibrate():
-    image = read_image('../data/mire_2.png')
+    image_1 = read_image('../data/mire_1.png')
+    image_2 = read_image('../data/mire_2.png')
 
-    _, chessboard_corners = cv2.findChessboardCorners(image, patternSize=CHESSBOARD_SIZE)
-    cv2.drawChessboardCorners(image, patternSize=CHESSBOARD_SIZE, corners=chessboard_corners, patternWasFound=_)
+    _1, chessboard_corners_1 = cv2.findChessboardCorners(image_1, patternSize=CHESSBOARD_SIZE)
+    _2, chessboard_corners_2 = cv2.findChessboardCorners(image_2, patternSize=CHESSBOARD_SIZE)
+    cv2.drawChessboardCorners(image_1, patternSize=CHESSBOARD_SIZE, corners=chessboard_corners_1, patternWasFound=_1)
+    cv2.drawChessboardCorners(image_2, patternSize=CHESSBOARD_SIZE, corners=chessboard_corners_2, patternWasFound=_2)
     if SAVE_IMAGES:
-        cv2.imwrite('../data/chessboard_corners.png', image)
-    coord_px = np.flip(np.squeeze(chessboard_corners))
-    coord_mm_x, coord_mm_y = np.meshgrid(20*np.arange(0, CHESSBOARD_SIZE[0]), 20*np.arange(0, CHESSBOARD_SIZE[1]))
-    coord_mm_x = np.ravel(coord_mm_x)
-    a=1
+        cv2.imwrite('../data/chessboard_corners_1.png', image_1)
+        cv2.imwrite('../data/chessboard_corners_2.png', image_2)
+    coord_px_1 = np.flip(np.squeeze(chessboard_corners_1))
+    coord_px_2 = np.flip(np.squeeze(chessboard_corners_2))
+    coord_px = np.concatenate((coord_px_1, coord_px_2), axis=0)
+
+
+    coord_mm_y_1, coord_mm_x_1 = np.meshgrid(20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1]))
+    coord_mm_1 = np.array([np.ravel(coord_mm_x_1), np.ravel(coord_mm_y_1), np.ravel(coord_mm_x_1)]).T
+
+    coord_mm_y_2, coord_mm_x_2 = np.meshgrid(20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1]))
+    coord_mm_2 = np.array([np.ravel(coord_mm_x_2), np.ravel(coord_mm_y_2), -100*np.ones_like(np.ravel(coord_mm_x_2))]).T
+
+    coord_mm = np.concatenate((coord_mm_1, coord_mm_2), axis=0)
+
+    A = get_A_matrix(coord_px, coord_mm, P_POINT_X, P_POINT_Y)
+    U = np.expand_dims(coord_px[:,0] - P_POINT_X, axis=0)
+    A_inv = np.linalg.pinv(A)
+
+    L = A_inv@U.T
+    params = get_parameters_from_L(np.ravel(L))
+
+    debug = True
+
+
 if __name__ == "__main__":
     calibrate()
--- srcs/utils/utils.py
+++ srcs/utils/utils.py
@@ -1,8 +1,49 @@
 from pathlib import Path
 import cv2
+import numpy as np
+
 
 def read_image(path_image):
-
     assert Path(path_image).exists(), "Image not found"
     return cv2.imread(path_image)
 
+
+def get_A_matrix(u: np.array, x: np.array, pp_x: float, pp_y) -> np.array:
+    U = np.array([
+        u[:, 1] - pp_y,
+        u[:, 1] - pp_y,
+        u[:, 1] - pp_y,
+        u[:, 1] - pp_y,
+        -(u[:, 0] - pp_x),
+        -(u[:, 0] - pp_x),
+        -(u[:, 0] - pp_x),
+    ]).T
+
+    X = np.array([
+        x[:, 0],
+        x[:, 1],
+        x[:, 2],
+        np.ones_like(x[:, 0]),
+        x[:, 0],
+        x[:, 1],
+        x[:, 2],
+    ]).T
+
+    A = np.multiply(U, X)
+    return A
+
+
+def get_parameters_from_L(L):
+
+    u = 1000*L
+    o2c_abs = 1/np.linalg.norm(L[-3:])
+    beta = o2c_abs*np.linalg.norm(L[:3])
+    o1c = L[3]*o2c_abs/beta
+    r_11 = L[0]*o2c_abs/beta
+    r_12 = L[1]*o2c_abs/beta
+    r_13 = L[2]*o2c_abs/beta
+    r_21 = L[4]*o2c_abs
+    r_22 = L[5]*o2c_abs
+    r_23 = L[6]*o2c_abs
+
+    debug = True
