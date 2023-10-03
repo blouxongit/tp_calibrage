23b27a6e0bfd712efca8b8f299c869f6b42d963a
Blouxongit <mr.bloux@gmail.com>
2023-10-03 18:31:10 +0200

    follow up

--- srcs/calibrage.py
+++ srcs/calibrage.py
@@ -4,43 +4,99 @@ from matplotlib import pyplot as plt
 from typing import List, Union, Dict, Tuple
 from PIL import Image
 
-from srcs.utils.utils import read_image, get_A_matrix, get_parameters_from_L
-from srcs.utils.constants import CHESSBOARD_SIZE, P_POINT_X, P_POINT_Y
+from srcs.utils.utils import (
+    read_image,
+    get_A_matrix,
+    get_parameters_from_L,
+    get_angles_from_matrix,
+    get_B_matrix,
+    get_R_matrix,
+    get_intrinsics,
+    get_extrinsics, get_transformation_matrix,
+)
+from srcs.utils.constants import CHESSBOARD_SIZE, P_POINT_X, P_POINT_Y, FOCAL_LENGTH
 
-SAVE_IMAGES = True
+SAVE_IMAGES = False
 
 
 def calibrate():
-    image_1 = read_image('../data/mire_1.png')
-    image_2 = read_image('../data/mire_2.png')
+    image_1 = read_image("../data/mire_1.png")
+    image_2 = read_image("../data/mire_2.png")
 
     _1, chessboard_corners_1 = cv2.findChessboardCorners(image_1, patternSize=CHESSBOARD_SIZE)
     _2, chessboard_corners_2 = cv2.findChessboardCorners(image_2, patternSize=CHESSBOARD_SIZE)
     cv2.drawChessboardCorners(image_1, patternSize=CHESSBOARD_SIZE, corners=chessboard_corners_1, patternWasFound=_1)
     cv2.drawChessboardCorners(image_2, patternSize=CHESSBOARD_SIZE, corners=chessboard_corners_2, patternWasFound=_2)
     if SAVE_IMAGES:
-        cv2.imwrite('../data/chessboard_corners_1.png', image_1)
-        cv2.imwrite('../data/chessboard_corners_2.png', image_2)
+        cv2.imwrite("../data/chessboard_corners_1.png", image_1)
+        cv2.imwrite("../data/chessboard_corners_2.png", image_2)
+
     coord_px_1 = np.flip(np.squeeze(chessboard_corners_1))
     coord_px_2 = np.flip(np.squeeze(chessboard_corners_2))
     coord_px = np.concatenate((coord_px_1, coord_px_2), axis=0)
 
+    coord_mm_y_1, coord_mm_x_1 = np.meshgrid(
+        20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1])
+    )
+    coord_mm_1 = np.array([np.ravel(coord_mm_x_1), np.ravel(coord_mm_y_1), np.zeros_like(np.ravel(coord_mm_x_1))]).T
 
-    coord_mm_y_1, coord_mm_x_1 = np.meshgrid(20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1]))
-    coord_mm_1 = np.array([np.ravel(coord_mm_x_1), np.ravel(coord_mm_y_1), np.ravel(coord_mm_x_1)]).T
-
-    coord_mm_y_2, coord_mm_x_2 = np.meshgrid(20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1]))
-    coord_mm_2 = np.array([np.ravel(coord_mm_x_2), np.ravel(coord_mm_y_2), -100*np.ones_like(np.ravel(coord_mm_x_2))]).T
+    coord_mm_y_2, coord_mm_x_2 = np.meshgrid(
+        20 * np.arange(0, CHESSBOARD_SIZE[0]), 20 * np.arange(0, CHESSBOARD_SIZE[1])
+    )
+    coord_mm_2 = np.array(
+        [np.ravel(coord_mm_x_2), np.ravel(coord_mm_y_2), -100 * np.ones_like(np.ravel(coord_mm_x_2))]
+    ).T
 
     coord_mm = np.concatenate((coord_mm_1, coord_mm_2), axis=0)
 
     A = get_A_matrix(coord_px, coord_mm, P_POINT_X, P_POINT_Y)
-    U = np.expand_dims(coord_px[:,0] - P_POINT_X, axis=0)
-    A_inv = np.linalg.pinv(A)
+    U = np.expand_dims(coord_px[:, 0] - P_POINT_X, axis=0)
+    A_pseudoinv = np.linalg.pinv(A)
 
-    L = A_inv@U.T
-    params = get_parameters_from_L(np.ravel(L))
+    L = A_pseudoinv @ U.T
+    o2c, beta, o1c, rotation_matrix = get_parameters_from_L(np.ravel(L))
+    yaw, pitch, roll = get_angles_from_matrix(rotation_matrix)
 
+    B = get_B_matrix(
+        coord_px,
+        coord_mm,
+        P_POINT_Y,
+        r_21=rotation_matrix[1, 0],
+        r_22=rotation_matrix[1, 1],
+        r_23=rotation_matrix[1, 2],
+        o2c=o2c,
+    )
+
+    R = get_R_matrix(
+        coord_px,
+        coord_mm,
+        P_POINT_Y,
+        r_31=rotation_matrix[2, 0],
+        r_32=rotation_matrix[2, 1],
+        r_33=rotation_matrix[2, 2],
+    )
+
+    o3c, f_2 = np.linalg.pinv(B) @ R
+    s_2 = FOCAL_LENGTH / f_2
+    s_1 = s_2 / beta
+
+    M_int = get_intrinsics(FOCAL_LENGTH, s_1, s_2, P_POINT_X, P_POINT_Y)
+    M_ext = get_extrinsics(rotation_matrix=rotation_matrix, translation_vector=np.array([o1c, o2c, o3c]))
+    M = get_transformation_matrix(M_intrinsics=M_int, M_extrinsics=M_ext)
+
+    coords_to_check_mm = np.c_[coord_mm[:88, :], np.ones((88,1))]
+    coords = (M@coords_to_check_mm.T).T
+
+    for i in range(88):
+        coords[i] = coords[i]/coords[i,-1]
+
+    X = coords[:,0]
+    Y = coords[:,1]
+
+    im = plt.imread("../data/mire_1.png")
+    plt.plot(X,Y, 's')
+    plt.imshow(im)
+    plt.show()
     debug = True
 
 
--- srcs/utils/utils.py
+++ srcs/utils/utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import List
+
 import cv2
 import numpy as np
 
@@ -9,41 +11,105 @@ def read_image(path_image):
 
 
 def get_A_matrix(u: np.array, x: np.array, pp_x: float, pp_y) -> np.array:
-    U = np.array([
-        u[:, 1] - pp_y,
-        u[:, 1] - pp_y,
-        u[:, 1] - pp_y,
-        u[:, 1] - pp_y,
-        -(u[:, 0] - pp_x),
-        -(u[:, 0] - pp_x),
-        -(u[:, 0] - pp_x),
-    ]).T
+    U = np.array(
+        [
+            u[:, 1] - pp_y,
+            u[:, 1] - pp_y,
+            u[:, 1] - pp_y,
+            u[:, 1] - pp_y,
+            -(u[:, 0] - pp_x),
+            -(u[:, 0] - pp_x),
+            -(u[:, 0] - pp_x),
+        ]
+    ).T
 
-    X = np.array([
-        x[:, 0],
-        x[:, 1],
-        x[:, 2],
-        np.ones_like(x[:, 0]),
-        x[:, 0],
-        x[:, 1],
-        x[:, 2],
-    ]).T
+    X = np.array(
+        [
+            x[:, 0],
+            x[:, 1],
+            x[:, 2],
+            np.ones_like(x[:, 0]),
+            x[:, 0],
+            x[:, 1],
+            x[:, 2],
+        ]
+    ).T
 
     A = np.multiply(U, X)
     return A
 
 
-def get_parameters_from_L(L):
+def get_parameters_from_L(L: np.array):
+    # In our case o2c is the same sign as o2c_abs
+    o2c = 1 / np.linalg.norm(L[-3:])
+    beta = o2c * np.linalg.norm(L[:3])
+    o1c = L[3] * o2c / beta
 
-    u = 1000*L
-    o2c_abs = 1/np.linalg.norm(L[-3:])
-    beta = o2c_abs*np.linalg.norm(L[:3])
-    o1c = L[3]*o2c_abs/beta
-    r_11 = L[0]*o2c_abs/beta
-    r_12 = L[1]*o2c_abs/beta
-    r_13 = L[2]*o2c_abs/beta
-    r_21 = L[4]*o2c_abs
-    r_22 = L[5]*o2c_abs
-    r_23 = L[6]*o2c_abs
+    r_11 = L[0] * o2c / beta
+    r_12 = L[1] * o2c / beta
+    r_13 = L[2] * o2c / beta
+    r_21 = L[4] * o2c
+    r_22 = L[5] * o2c
+    r_23 = L[6] * o2c
+    r_31, r_32, r_33 = np.cross(np.array([r_11, r_12, r_13]), np.array([r_21, r_22, r_23]))
 
-    debug = True
+    rotation_matrix = np.array(
+        [
+            [r_11, r_12, r_13],
+            [r_21, r_22, r_23],
+            [r_31, r_32, r_33],
+        ]
+    )
+    return o2c, beta, o1c, rotation_matrix
+
+
+def get_angles_from_matrix(M):
+    yaw = -np.arctan(M[1, 2] / M[2, 2])
+    pitch = -np.arctan(M[0, 1] / M[0, 0])
+    roll = -np.arctan(M[0, 2] / (-M[1, 2] * np.sin(yaw) + M[2, 2] * np.cos(yaw)))
+    return yaw, pitch, roll
+
+
+def get_B_matrix(u, x, pp_y, r_21, r_22, r_23, o2c):
+    U = np.array(
+        [
+            u[:, 1] - pp_y,
+        ]
+    ).T
+
+    construct = -(r_21 * x[:, 0] + r_22 * x[:, 1] + r_23 * x[:, 2] + o2c)
+    construct = np.expand_dims(construct, axis=1)
+    test = np.c_[U, construct]
+    return np.c_[U, construct]
+
+
+def get_R_matrix(u, x, pp_y, r_31, r_32, r_33):
+    U = np.array(
+        [
+            -(u[:, 1] - pp_y),
+        ]
+    ).T
+    construct = r_31 * x[:, 0] + r_32 * x[:, 1] + r_33 * x[:, 2]
+    construct = np.expand_dims(construct, axis=1)
+    return np.multiply(U, construct)
+
+
+def get_extrinsics(rotation_matrix, translation_vector):
+    M = np.eye(4)
+    M[:3, :3] = rotation_matrix
+    M[:3, -1] = translation_vector
+    return M
+
+
+def get_intrinsics(f, s_1, s_2, i_1, i_2):
+    return np.array(
+        [
+            [f / s_1, 0, i_1, 0],
+            [0, f / s_2, i_2, 0],
+            [0, 0, 1, 0],
+        ]
+    )
+
+
+def get_transformation_matrix(M_intrinsics, M_extrinsics):
+    return M_intrinsics @ M_extrinsics
